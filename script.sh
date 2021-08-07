9a77df8b9f3b1f3b522a69758aafd3bb6f3160c1
volcano <volcano@mail.com>
2021-08-08 04:08:35 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+cp ~/.config/Code/User/settings.json vs-code-stuffs/settings.json
+cp ~/.zshrc command-line/.zshrc
+cp ~/.bashrc command-line/.bashrc
+cp ~/.vimrc command-line/.vimrc
+cp ~/.ideavimrc Android-studio/.ideavimrc
