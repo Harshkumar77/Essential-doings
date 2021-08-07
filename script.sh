ecfd5fabcd05cbf07fd9f35d4b5d3846347c01fc
volcano <volcano@mail.com>
2021-08-08 04:16:51 +0530

    Nth commit

--- script.sh
+++ script.sh
@@ -3,3 +3,7 @@ cp ~/.zshrc command-line/.zshrc
 cp ~/.bashrc command-line/.bashrc
 cp ~/.vimrc command-line/.vimrc
 cp ~/.ideavimrc Android-studio/.ideavimrc
+
+git add .
+git commit -m "Nth commit"
+git push
