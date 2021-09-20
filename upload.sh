d420695a5d9053104078aa68c0998ed63517e1a9
mighty_volcano <harshkumar@noreply.com>
2021-09-20 20:26:18 +0530

    Nth commit

--- upload.sh
+++ upload.sh
@@ -1,10 +1,9 @@
-pv ~/.config/Code/User/settings.json >  vs-code-stuffs/settings.json
-pv ~/.zshrc >  command-line/.zshrc
-pv ~/.bashrc >  command-line/.bashrc
-pv ~/.vimrc > command-line/.vimrc
-pv ~/.ideavimrc > Android-studio/.ideavimrc
-pv ~/.gitconfig > command-line/.gitconfig
-cp ~/.vscode/ > .vscode/ -r
+cp ~/.config/Code/User/settings.json >  vs-code-stuffs/settings.json
+cp ~/.zshrc >  command-line/.zshrc
+cp ~/.bashrc >  command-line/.bashrc
+cp ~/.vimrc > command-line/.vimrc
+cp ~/.ideavimrc > Android-studio/.ideavimrc
+cp ~/.gitconfig > command-line/.gitconfig
 git add .
 git commit -m "Nth commit"
 git push
